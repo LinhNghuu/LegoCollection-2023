5afecdf934bc5786cd4072a09620b5d22d6cce29
LinhNgHuu <linh.nghuu3110@gmail.com>
2023-11-09 10:42:49 -0500

    AS5-part2

--- modules/legoSets.js
+++ modules/legoSets.js
@@ -1,3 +1,4 @@
+/*
 const setData = require('../data/setData');
 const themeData = require('../data/themeData');
 
@@ -57,6 +58,94 @@ module.exports = {
   getSetByNum,
   getSetsByTheme,
 };
+ */
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+// Create a new Sequelize instance
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  logging: false
+});
+
+// Define the Theme model
+const Theme = sequelize.define('Theme', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: Sequelize.STRING
+}, {
+  timestamps: false
+});
+
+// Define the Set model
+const Set = sequelize.define('Set', {
+  set_num: {
+    type: Sequelize.STRING,
+    primaryKey: true
+  },
+  name: Sequelize.STRING,
+  year: Sequelize.INTEGER,
+  num_parts: Sequelize.INTEGER,
+  theme_id: Sequelize.INTEGER,
+  img_url: Sequelize.STRING
+}, {
+  timestamps: false
+});
+
+// Associate the models
+Set.belongsTo(Theme, { foreignKey: 'theme_id' });
+
+function initialize() {
+  return sequelize.sync().catch(err => {
+    throw err;
+  });
+}
+
+function getAllSets() {
+  return Set.findAll({ include: [Theme] });
+}
+
+function getSetByNum(setNum) {
+  return Set.findAll({
+    include: [Theme],
+    where: { set_num: setNum }
+  }).then(sets => {
+    if (sets.length > 0) return sets[0];
+    else throw new Error("Unable to find requested set");
+  });
+}
+
+function getSetsByTheme(theme) {
+  return Set.findAll({
+    include: [Theme],
+    where: {
+      '$Theme.name$': {
+        [Sequelize.Op.iLike]: `%${theme}%`
+      }
+    }
+  }).then(sets => {
+    if (sets.length > 0) return sets;
+    else throw new Error("Unable to find requested sets");
+  });
+}
+
+module.exports = {
+  initialize,
+  getAllSets,
+  getSetByNum,
+  getSetsByTheme
+};
+
 
 /*
 // Testing the functions
